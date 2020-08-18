fae305db4bdc508c166558d6af6fee78d288c20a
Vuchke <vuchke@gmail.com>
2020-08-18 18:16:31 +0200

    2nd

--- carousel.js
+++ carousel.js
@@ -1,24 +1,24 @@
-$('.owl-carousel').owlCarousel({
-  navText : ["",""],
-  rewindNav : true,
-  loop:true,
-  margin:10,
+$(".owl-carousel").owlCarousel({
+  navText: ["", ""],
+  rewindNav: true,
+  loop: true,
+  margin: 10,
   nav: true,
   lazyLoad: true,
   autoplay: true,
-  slideTransition: 'ease-in-out',
-  autoplaySpeed: 1500,
+  autoplaySpeed: 1200,
+  autoplayHoverPause: true,
+  slideTransition: "ease-in-out",
   navSpeed: 1500,
-  responsive:{
-      0:{
-          items:1
-      },
-      650:{
-          items:2
-      },
-      1000:{
-          items:3
-      }
-  }
-})
-
+  responsive: {
+    0: {
+      items: 1,
+    },
+    650: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
